42722acc52f4f4cf5bd273824380f86feffa77f5
noname552A <unknown.geomentry@gmail.com>
2022-04-25 10:32:50 +0700

    hotfix

--- lib/view/sertifikat.dart
+++ lib/view/sertifikat.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:date_format/date_format.dart';
 import 'package:provider/provider.dart';
 import 'package:taniku/viewmodel/dbdatamod.dart';
 
